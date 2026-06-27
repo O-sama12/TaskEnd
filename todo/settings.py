221946d9b73fe3b2866c86e91bcf212e10cbedfc
Osama Abdul Qader <oaqader4@gmail.com>
2026-06-27 17:00:10 +0530

    safeguarded the secrets

--- todo/settings.py
+++ todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0g6s^%p*_+*=b1prax4^_e=5h$ty=e4swddsk)=yn60#ko_74_'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "taskend_db",
-        'USER': "taskend_user",
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("taskend_user"),
         "PASSWORD":os.getenv("DB_PASS"),
-        "HOST": "localhost",
-        "PORT": "5432",
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
