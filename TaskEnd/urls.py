82357b7b79aaf6e277f85e15c82433c2f2a1f718
Osama Abdul Qader <oaqader4@gmail.com>
2026-06-29 14:48:07 +0530

    Created Serializer and Implemented Task REST API with GET and POST using DRF

--- TaskEnd/urls.py
+++ TaskEnd/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import landing, signup, login_view, forget_pass, tasks, complete_task, delete_task, logout_view
+from .views import landing, signup, login_view, forget_pass, tasks, complete_task, delete_task, logout_view, TaskListAPIView
 
 urlpatterns = [
     path('', landing, name='landing'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('complete/<int:task_id>/', complete_task, name='complete_task'),
     path('delete/<int:task_id>/', delete_task, name='delete_task'),
     path('logout/', logout_view, name='logout'),
-    path("accounts/", include("allauth.urls"))
+    path("accounts/", include("allauth.urls")),
+    path("api/tasks/", TaskListAPIView.as_view(), name= "api_tasks"),
 ]
--- TaskEnd/views.py
+++ TaskEnd/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 from django.utils.timezone import now
 from django.db import IntegrityError
 from django.contrib import messages
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import TaskSerializer
 from .models import Task
 # Create your views here.
 def landing(request):
@@ -74,3 +77,14 @@ def delete_task(request, task_id):
 def logout_view(request):
     logout(request)
     return redirect("landing")
+class TaskListAPIView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many = True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = TaskSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = 201)
+        return Response(serializer.errors, status=400)
